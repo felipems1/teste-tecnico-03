dd8496f829db75c6d568dc25e40464f326450935
felipe <083felype@gmail.com>
2024-01-10 02:25:39 -0300

    feat: add delete-location.service

---
+++ src/services/delete-location.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+
+@Injectable()
+export class DeleteLocationService {
+  constructor(private prisma: PrismaService) {}
+
+  async deleteLocation(locationId: string) {
+    const existingLocation = await this.prisma.location.findUnique({
+      where: { id: locationId },
+    })
+
+    if (!existingLocation) {
+      throw new NotFoundException('Location with ID not found')
+    }
+
+    return this.prisma.location.delete({
+      where: { id: locationId },
+    })
+  }
+}
