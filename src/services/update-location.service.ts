a67da7bb88a36e480ddf85f11d1bd170edfd3183
felipe <083felype@gmail.com>
2024-01-10 04:32:56 -0300

    feat: add update-location.service

---
+++ src/services/update-location.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+
+interface UpdateLocationType {
+  name: string
+  city: string
+  state: string
+}
+
+@Injectable()
+export class UpdateLocationService {
+  constructor(private prisma: PrismaService) {}
+
+  async updateLocation(locationId: string, updateData: UpdateLocationType) {
+    const existingLocation = await this.prisma.location.findUnique({
+      where: { id: locationId },
+    })
+
+    if (!existingLocation) {
+      throw new NotFoundException('Location with ID not found')
+    }
+
+    return this.prisma.location.update({
+      where: { id: locationId },
+      data: updateData,
+    })
+  }
+}
