78a6e8b2169c63ab9c482bb7f01934762f114776
felipe <083felype@gmail.com>
2024-04-16 02:07:42 -0300

    feat: add links navigation

---
+++ src/utils/links.tsx
@@ -0,0 +1,8 @@
+import { BedDouble, Bone, HomeIcon, Shapes } from 'lucide-react'
+
+export const links = [
+  { id: 1, path: '/camas', name: 'camas', icon: <BedDouble /> },
+  { id: 2, path: '/brinquedos', name: 'brinquedos', icon: <Shapes /> },
+  { id: 3, path: '/comedouros', name: 'comedouros', icon: <Bone /> },
+  { id: 4, path: '/casinhas', name: 'casinhas', icon: <HomeIcon /> },
+]
