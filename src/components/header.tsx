b849b9632dad689fc9088e07581a38ca9d4e2e24
felipe <083felype@gmail.com>
2024-04-15 23:57:38 -0300

    feat: add Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,22 @@
+import { Search, ShoppingCart } from 'lucide-react'
+
+export function Header() {
+  return (
+    <header className="w-full h-32 bg-pink-600 text-white p-4 flex flex-col justify-center gap-4">
+      <div className="flex justify-between items-center">
+        <h2>Pet Friends Acessories</h2>{' '}
+        <div className="flex p-2 justify-center items-center rounded bg-pink-700 cursor-pointer hover:bg-pink-500">
+          <ShoppingCart />
+        </div>
+      </div>
+      <form className="flex w-full items-center gap-3 rounded bg-white px-5 py-3 ring-zinc-700">
+        <input
+          type="text"
+          placeholder="O que você procura?"
+          className="flex-1 bg-transparent text-sm outline-none text-black placeholder:text-zinc-500"
+        />
+        <Search className="h-5 w-5 text-zinc-500" />
+      </form>
+    </header>
+  )
+}
