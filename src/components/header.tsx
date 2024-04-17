ff6915226097f56d7b3ca199be57f9a73b36950c
felipe <083felype@gmail.com>
2024-04-17 01:47:23 -0300

    feat: add SearchForm component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,14 @@
-import { Search } from 'lucide-react'
+import Link from 'next/link'
 import { CartWidget } from './cart-widget'
+import { SearchForm } from './search-form'
 
 export function Header() {
   return (
     <header className="w-full h-32 bg-purple-600 text-white p-4 flex flex-col justify-center gap-4">
       <div className="flex justify-between items-center">
-        <h2>Pet Friends Acessories</h2> <CartWidget />
+        <Link href="/">Pet Friends Acessories</Link> <CartWidget />
       </div>
-      <form className="flex w-full items-center gap-3 rounded bg-white px-5 py-3 ring-zinc-700">
-        <input
-          type="text"
-          placeholder="O que você procura?"
-          className="flex-1 bg-transparent text-sm outline-none text-black placeholder:text-zinc-500"
-        />
-        <Search className="h-5 w-5 text-zinc-500" />
-      </form>
+      <SearchForm />
     </header>
   )
 }
